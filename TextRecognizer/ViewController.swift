9e2fe2e16a7670d7cb76380ff2fa05ee12a12960
G. Moraleda <guillermo.moraleda@cluno.com>
2019-11-04 14:19:37 +0100

    Initial Commit

---
+++ TextRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognizer
+//
+//  Created by Guillermo Moraleda on 04.11.19.
+//  Copyright © 2019 Guillermo Moraleda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
